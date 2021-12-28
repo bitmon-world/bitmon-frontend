a5713644fb26dd412ece99084e5364832743ab74
eabz <eabz@polispay.org>
2021-12-27 18:06:34 -0600

    WIP

--- components/Builder/index.tsx
+++ components/Builder/index.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import Image from "next/image";
+import { ButtonBlue, ButtonBlueDisabled } from "../Button";
 
 export interface TrainerAttributes {
   body_type: "male" | "female" | null;
@@ -9,7 +10,8 @@ export interface TrainerAttributes {
 export const TrainerBuilder: FC<{
   attributes: TrainerAttributes;
   setAttributes: (data: TrainerAttributes) => void;
-}> = ({ attributes, setAttributes }) => {
+  toggleTitle: (toggle: boolean) => void;
+}> = ({ attributes, setAttributes, toggleTitle }) => {
   return !attributes.body_type ? (
     <div>
       <div className="flex flex-row items-center justify-between bg-contain bg-no-repeat bg-center bg-title-background h-[54px] mx-auto">
@@ -31,9 +33,10 @@ export const TrainerBuilder: FC<{
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 w-2/4 mx-auto mt-8">
         <button
-          onClick={() =>
-            setAttributes({ body_type: "female", body_color: null })
-          }
+          onClick={() => {
+            setAttributes({ body_type: "female", body_color: null });
+            toggleTitle(false);
+          }}
         >
           <div className="flex items-center justify-center cursor-pointer hover:drop-shadow-lg hover:shadow-black">
             <Image
@@ -46,7 +49,10 @@ export const TrainerBuilder: FC<{
           </div>
         </button>
         <button
-          onClick={() => setAttributes({ body_type: "male", body_color: null })}
+          onClick={() => {
+            setAttributes({ body_type: "male", body_color: null });
+            toggleTitle(false);
+          }}
         >
           <div className="flex items-center justify-center cursor-pointer hover:drop-shadow-lg hover:shadow-black">
             <Image
@@ -62,40 +68,23 @@ export const TrainerBuilder: FC<{
     </div>
   ) : (
     <div>
-      <div className="flex flex-row items-center justify-between bg-contain bg-no-repeat bg-center bg-title-background h-[48px] mx-auto">
-        <div className="flex flex-row items-center gap-x-10 items-center mx-auto">
-          <div>
-            <span
-              className="bg-orange text-white text-6xl"
-              style={{ fontFamily: "Candal" }}
-            >
-              02
-            </span>{" "}
-          </div>
-          <div className="text-2xl">
-            <span className="text-white">
-              Choose your <span className="text-orange">attributes</span>
-            </span>
-          </div>
-        </div>
-      </div>
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-x-5 w-2/3 mx-auto mt-8">
-        <div>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-x-5 w-4/5 mx-auto mt-12">
+        <div className="flex flex-row justify-center md:mt-20">
           {!attributes.body_color ? (
             attributes.body_type === "male" ? (
               <Image
-                className={"rounded-lg"}
+                className={"rounded-lg mt-10"}
                 src="/img/male-body.png"
-                width="300"
-                height="338"
+                width="380"
+                height="428"
                 alt={"Bitmon Trainer Male Body"}
               />
             ) : (
               <Image
-                className={"rounded-lg"}
+                className={"rounded-lg mt-10"}
                 src="/img/female-body.png"
-                width="300"
-                height="338"
+                width="380"
+                height="428"
                 alt={"Bitmon Trainer Female Body"}
               />
             )
@@ -103,11 +92,88 @@ export const TrainerBuilder: FC<{
             <div />
           )}
         </div>
-        <div>
+        <div className="col-span-2">
           <div></div>
-          <div className="bg-white rounded-lg">
-            <div>
-
+          <div>
+            <div className="flex flex-row items-center justify-between bg-contain bg-no-repeat bg-center bg-title-background h-[48px] mx-auto">
+              <div className="flex flex-row items-center gap-x-10 items-center mx-auto">
+                <div>
+                  <span
+                    className="bg-orange text-white text-4xl md:text-6xl"
+                    style={{ fontFamily: "Candal" }}
+                  >
+                    02
+                  </span>{" "}
+                </div>
+                <div className="text-xl">
+                  <span className="text-white">
+                    Choose your <span className="text-orange">attributes</span>
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="bg-white rounded-lg">
+              <div></div>
+            </div>
+            <div className="flex flex-row justify-end gap-x-3 mt-5">
+              <div>
+                <div
+                  className={
+                    "w-22 mx-auto rounded-full border-2 shadow shadow-orange bg-orange border-orange"
+                  }
+                >
+                  <button
+                    className={
+                      "px-1 py-1 text-white text-xl uppercase border-2 border-black rounded-full relative w-full"
+                    }
+                  >
+                    <div className="flex flex-row items-center justify-center">
+                      <Image
+                        src="/icons/builder/dice.svg"
+                        width="25"
+                        height="25"
+                      />
+                    </div>
+                  </button>
+                </div>
+              </div>
+              <div>
+                <div
+                  className={
+                    "w-22 mx-auto rounded-full border-2 shadow shadow-orange bg-orange border-orange"
+                  }
+                >
+                  <button
+                    className={
+                      "px-1 py-1 text-white text-xl uppercase border-2 border-black rounded-full relative w-full"
+                    }
+                    onClick={() =>
+                      setAttributes({
+                        body_type: attributes.body_type,
+                        body_color: null,
+                      })
+                    }
+                  >
+                    <div className="flex flex-row items-center justify-center">
+                      <Image
+                        src="/icons/builder/restart.svg"
+                        width="25"
+                        height="25"
+                      />
+                    </div>
+                  </button>
+                </div>
+              </div>
+              <div>
+                {attributes.body_type && attributes.body_color ? (
+                  <ButtonBlue
+                    text="Finish"
+                    onClick={() => console.log("finish")}
+                  />
+                ) : (
+                  <ButtonBlueDisabled text={"Finish"} />
+                )}
+              </div>
             </div>
           </div>
         </div>
--- pages/creator/[id].tsx
+++ pages/creator/[id].tsx
@@ -47,34 +47,44 @@ export default function CreatorSingle(): JSX.Element {
     setAttributes(data);
   }
 
+  const [showTitle, setShowTitle] = useState(true);
+
+  function toggleTitle(toggle: boolean) {
+    setShowTitle(toggle);
+  }
+
   return (
     <div className="relative z-10 mx-4 h-full pb-10">
-      <div className="pt-14 text-center flex flex-row justify-center items-center gap-x-10">
-        <div className="hidden md:inline-flex ml-10">
-          <Image
-            src="/img/separator-right.svg"
-            width="250"
-            height="17"
-            alt="Bitmon Separator"
-          />
+      {showTitle ? (
+        <div className="pt-14 text-center flex flex-row justify-center items-center gap-x-10">
+          <div className="hidden md:inline-flex ml-10">
+            <Image
+              src="/img/separator-right.svg"
+              width="250"
+              height="17"
+              alt="Bitmon Separator"
+            />
+          </div>
+          <div>
+            <Image
+              src="/img/trainer-creator.png"
+              width="200"
+              height="85"
+              alt="Bitmon Separator"
+            />
+          </div>
+          <div className="hidden md:inline-flex mr-10">
+            <Image
+              src="/img/separator-left.svg"
+              width="250"
+              height="17"
+              alt="Bitmon Separator"
+            />
+          </div>
         </div>
-        <div>
-          <Image
-            src="/img/trainer-creator.png"
-            width="200"
-            height="85"
-            alt="Bitmon Separator"
-          />
-        </div>
-        <div className="hidden md:inline-flex mr-10">
-          <Image
-            src="/img/separator-left.svg"
-            width="250"
-            height="17"
-            alt="Bitmon Separator"
-          />
-        </div>
-      </div>
+      ) : (
+        <div />
+      )}
       <div className="mt-8">
         {wallet.connected ? (
           metadata ? (
@@ -82,6 +92,7 @@ export default function CreatorSingle(): JSX.Element {
               <TrainerBuilder
                 attributes={attributes}
                 setAttributes={attributesUpdate}
+                toggleTitle={toggleTitle}
               />
             ) : (
               <div className="text-center">
