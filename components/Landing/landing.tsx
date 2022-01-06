d9feaf8d8a8a7e9ef59761e35a9c95be66cf15b4
eabz <eabz@polispay.org>
2022-01-06 11:55:06 -0600

    WIP

--- components/Landing/landing.tsx
+++ components/Landing/landing.tsx
@@ -37,9 +37,58 @@ export const Landing = () => {
           </h1>
         </div>
       </div>
-
-      <div id="about"></div>
-      <div></div>
+      <div id="about" className="bg-light-green">
+        <div className="flex flex-row justify-end pt-7">
+          <Image src="/bitmons/viscat.png" width="300" height="214" />
+        </div>
+        <div className="mx-8 md:mx-20 lg:mx-40 py-12 -mt-16 md:-mt-32 lg:-mt-40">
+          <h1 className="text-orange text-xl">About us</h1>
+          <h1 className="text-4xl">What is Bitmon?</h1>
+          <div className="pb-20 max-w-3xl text-justify">
+            <p className="py-4">
+              Bitmon is a P2E game made in a collaboration by Kindynos, Vibing
+              Studios and Maki Studios, where you can play, breed, trade and
+              fight with decentralized monsters.
+            </p>
+            <p className="py-4">
+              Bitmon is a collection of 10k Bitmon PFP trainers that will serve
+              as an access to the BitmonDAO.
+            </p>
+            <p className="py-4">
+              Bitmon was conceived from the vision that anyone can create a
+              video game based on it since CC0 rights of the Bitmons will be
+              given so that any person, creator, artist, or developer can make a
+              product from the art and mechanics of the video game. People will
+              be able to request funding from the DAO, which will be controlled
+              by the bitmon trainers, to make art derivations from it, examples
+              can be an anime, web series, manga, videogame, trading card game,
+              or anything bitmon related. The first decentralized videogame
+              brand.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div id="whitepaper" className="bg-purple">
+        <div className="mx-40 py-10 pt-32">
+          <h1 className="text-orange text-xl">Unlock your</h1>
+          <h1 className="text-4xl uppercase text-white">Trainer Potential</h1>
+          <div className="pb-20 max-w-3xl text-justify text-white">
+            <p className="py-4">
+              Bitmon wants to be the first video game created by the people, for
+              the people. With a treasury that will help hire and reward new art
+              products derived from it.
+            </p>
+            <p className="py-4">
+              The trainers will also get event passes to catch new bitmons,
+              breed their Bitmons, fight with them in the video game created by
+              the Bitmon Core team while also being able to receive airdrops
+              from new events, a token used for buying items and breeding, and
+              decide what to do with the treasury.
+            </p>
+            <div className="flex flex-row justify-end"></div>
+          </div>
+        </div>
+      </div>
       <div id="roadmap"></div>
       <div id="faq"></div>
     </div>
