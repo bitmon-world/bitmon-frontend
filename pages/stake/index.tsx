dad105daddbdc49085c1ee323d92b11c02305437
eabz <eabz@polispay.org>
2022-02-17 13:05:09 -0600

    WIP

--- pages/stake/index.tsx
+++ pages/stake/index.tsx
@@ -12,8 +12,19 @@ import { intersect } from "@hapi/hoek";
 import { TRAINER_MINTS } from "../../constants/mints";
 import { fetchTrainers } from "../../functions/fetch-trainers";
 import { MetadataData } from "@metaplex-foundation/mpl-token-metadata";
+import { TrainerImage } from "../../components/TrainerImage";
+import {
+  ButtonBlue,
+  ButtonBlueDisabled,
+  ButtonGreen,
+} from "../../components/Button";
 
 export default function Stake(): JSX.Element {
+  enum View {
+    STAKED,
+    UNSTAKED,
+  }
+
   const url =
     process.env.NEXT_PUBLIC_SOLANA_RPC || clusterApiUrl("mainnet-beta");
   const connect = createConnectionConfig(url);
@@ -41,6 +52,9 @@ export default function Stake(): JSX.Element {
     if (!wallet || !wallet.connected || !wallet.publicKey) return;
     fetch_tokens();
   }, [wallet]);
+
+  const [view, setView] = useState(View.UNSTAKED);
+
   return (
     <div className="relative z-10 mx-4 h-full pb-10">
       <div className="pt-14 text-center flex flex-row justify-center items-center gap-x-10">
@@ -73,21 +87,172 @@ export default function Stake(): JSX.Element {
         {wallet.connected ? (
           loading ? (
             <Loader />
-          ) : tokens.length > 0 ? (
+          ) : (
             <div className="text-center">
               <div className="flex flex-row items-center justify-center bg-contain bg-no-repeat bg-center bg-title-background h-[58px] mx-auto">
                 <p className="top-0 text-xl text-white">
-                  Stake your <span className="text-orange">trainers</span> to
-                  get some <span className="text-orange">$BIT</span>
+                  Stake your Bitmon <span className="text-orange">trainer</span>{" "}
+                  to earn <span className="text-orange">$BIT</span>
                 </p>
               </div>
-              <div className="z-10 mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 w-3/4 mx-auto"></div>
-            </div>
-          ) : (
-            <div className="flex flex-row items-center justify-center bg-contain bg-no-repeat bg-center bg-title-background h-[58px] mx-auto">
-              <p className="top-0 text-xl text-white">
-                No Bitmon trainers found.
-              </p>
+              <div className="flex flex-row items-center justify-center w-full my-6">
+                <div className="bg-white rounded-lg py-2 px-4">
+                  <h1 className="text-orange">Warning!</h1>
+                  <h1>
+                    You won't be able to use your trainers until you unstake
+                    them and wait for a 7 days lock period.
+                  </h1>
+                </div>
+              </div>
+              <div className="flex flex-row items-center justify-center mx-auto mt-2">
+                {view === View.STAKED ? (
+                  <>
+                    <div
+                      className={
+                        "mx-2 opacity-40 rounded-full border-2 shadow shadow-blue bg-blue border-blue"
+                      }
+                    >
+                      <button
+                        className={
+                          "py-1 px-6 text-white text-xl uppercase border-2 border-black rounded-full relative cursor-not-allowed"
+                        }
+                      >
+                        <div className="flex flex-row items-center justify-center">
+                          <div>
+                            <h1>Staked</h1>
+                          </div>
+                          <div className="absolute top-0 right-0 opacity-10">
+                            <Image
+                              src="/icons/bitmon-icon-white.svg"
+                              height="40"
+                              width="40"
+                            />
+                          </div>
+                        </div>
+                      </button>
+                    </div>
+                    <div
+                      className={
+                        "mx-2 rounded-full border-2 shadow shadow-blue bg-blue border-blue"
+                      }
+                    >
+                      <button
+                        className={
+                          "py-1 px-6 text-white text-xl uppercase border-2 border-black rounded-full relative"
+                        }
+                        onClick={() => setView(View.UNSTAKED)}
+                      >
+                        <div className="flex flex-row items-center justify-center">
+                          <div>
+                            <h1>Unstaked</h1>
+                          </div>
+                          <div className="absolute top-0 right-0 opacity-10">
+                            <Image
+                              src="/icons/bitmon-icon-white.svg"
+                              height="40"
+                              width="40"
+                            />
+                          </div>
+                        </div>
+                      </button>
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    <div
+                      className={
+                        "mx-2 rounded-full border-2 shadow shadow-blue bg-blue border-blue"
+                      }
+                    >
+                      <button
+                        className={
+                          "py-1 px-6 text-white text-xl uppercase border-2 border-black rounded-full relative"
+                        }
+                        onClick={() => setView(View.STAKED)}
+                      >
+                        <div className="flex flex-row items-center justify-center">
+                          <div>
+                            <h1>Staked</h1>
+                          </div>
+                          <div className="absolute top-0 right-0 opacity-10">
+                            <Image
+                              src="/icons/bitmon-icon-white.svg"
+                              height="40"
+                              width="40"
+                            />
+                          </div>
+                        </div>
+                      </button>
+                    </div>
+                    <div
+                      className={
+                        "opacity-40 mx-2 rounded-full border-2 shadow shadow-blue bg-blue border-blue"
+                      }
+                    >
+                      <button
+                        className={
+                          "py-1 px-6 text-white text-xl uppercase border-2 border-black rounded-full relative cursor-not-allowed"
+                        }
+                      >
+                        <div className="flex flex-row items-center justify-center">
+                          <div>
+                            <h1>Unstaked</h1>
+                          </div>
+                          <div className="absolute top-0 right-0 opacity-10">
+                            <Image
+                              src="/icons/bitmon-icon-white.svg"
+                              height="40"
+                              width="40"
+                            />
+                          </div>
+                        </div>
+                      </button>
+                    </div>
+                  </>
+                )}
+              </div>
+              {view === View.STAKED ? (
+                <div className="text-center mt-5">
+                  <div className="flex flex-row items-center justify-center">
+                    <div className="bg-purple rounded-lg py-2 px-4">
+                      <p className="text-xl text-white ">Staked Trainers</p>
+                    </div>
+                  </div>
+                  <div className="z-10 mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 w-3/4 mx-auto">
+                    {tokens.staked.map((t) => {
+                      return (
+                        <TrainerImage
+                          link
+                          key={t.mint}
+                          uri={t.data.uri}
+                          mint={t.mint}
+                        />
+                      );
+                    })}
+                  </div>
+                </div>
+              ) : null}
+              {view === View.UNSTAKED ? (
+                <div className="text-center mt-5">
+                  <div className="flex flex-row items-center justify-center">
+                    <div className="bg-purple rounded-lg py-2 px-4">
+                      <p className="text-xl text-white ">Unstaked Trainers</p>
+                    </div>
+                  </div>
+                  <div className="z-10 mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 w-3/4 mx-auto">
+                    {tokens.unstaked.map((t) => {
+                      return (
+                        <TrainerImage
+                          link
+                          key={t.mint}
+                          uri={t.data.uri}
+                          mint={t.mint}
+                        />
+                      );
+                    })}
+                  </div>
+                </div>
+              ) : null}
             </div>
           )
         ) : (
